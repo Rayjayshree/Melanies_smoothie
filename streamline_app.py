c3ceb1e5f0b8e063625f9d60bf6b5beba2a620dc
Rayjayshree <148844933+Rayjayshree@users.noreply.github.com>
2024-05-15 15:00:52 +0530

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -43,8 +43,9 @@ if ingredients_list:
     
     st.success('Your Smoothie is ordered, Betty Jean!', icon="✅")
     
-    import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response.json())
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
