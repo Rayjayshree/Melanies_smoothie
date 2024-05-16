cb0ad48ef35ee9b2afe534e463aa697338090ba3
Rayjayshree <148844933+Rayjayshree@users.noreply.github.com>
2024-05-16 11:32:20 +0530

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
+st.dataframe(pd_df)
 #st.stop()
 
 
@@ -40,7 +40,6 @@ if ingredients_list:
         
         st.subheader(fruit_chosen + 'Nutrition information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        st.text(fruityvice_response)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
